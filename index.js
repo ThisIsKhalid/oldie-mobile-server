bbdae3a34c55a8d542190476468d52addcf865e4
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-27 20:34:29 +0600

    my orders deleted

--- index.js
+++ index.js
@@ -189,6 +189,13 @@ async function run() {
       const result = await ordersCollection.find(filter).toArray();
       res.send(result)
     })
+
+    app.delete('/myorders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    })
   } finally {
   }
 }
